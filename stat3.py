813720f452aefefb75ab8f1001c88c63f80a7808
Cingis Alexander <kratochvil.ca@gmail.com>
2017-06-18 14:35:49 +0200

    now it shoul be working!

--- stat3.py
+++ stat3.py
@@ -4,4 +4,34 @@ Created on Sun Jun 18 14:02:01 2017
 
 @author: Cingis Alexander
 """
+import numpy as np
+data = open("vers.dat","r") 
+data =None
+prep_data = data.read()
+print(prep_data)
+pr_data = np.array(prep_data) 
+print(pr_data)
+type(pr_data[1])
+np.shape(pr_data)
+
+import csv
+with open("vers.csv") as csvfile:
+    #it will be readed as string
+    readCSV = csv.reader(csvfile, delimiter=" ")
+    out = readCSV
+    col0 = []
+    col1 = []
+    col2 = []
+    ct = 0
+    for row in readCSV:
+        if ct ==0:
+            ct =1 
+        else:
+            cole0 = row[0]
+            cole1 =row[1]
+            cole2 = row[2]
+            col0.append(cole0)
+            col1.append(cole1)
+            col2.append(cole2)
+        
 
